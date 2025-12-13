b21f3bf0a2e948169b60f4eed422566a4094efdb
fatemamoh <fatimabuzuhaira@gmail.com>
2025-12-13 20:46:09 +0300

    readjusting the review model again

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -9,27 +9,4 @@ const Review = require('../models/review');
 
 // create: 
 
-router.get('/' , async (req, res)=> {
-    try {
-        const finishedBooks = await Book.find({
-            owner: req.session.user._id,
-            status: 'Finished'
-        });
-        const reviewedBookIds = await Review.find({
-            author: req.session.user._id}, 'book');
-
-        const reviewedIds = reviewedBookIds.map(r => r.book.toString());    
-
-        const booksToReview = finishedBooks.filter(book => 
-            !reviewedIds.includes(book._id.toString())
-        );
-        res.render('reviews/index.ejs', { booksToReview,}); 
-    } 
-    
-    catch (error) {
-          console.error(error)
-        res.redirect('/book')
-    }
-})
-
 module.exports = router;
