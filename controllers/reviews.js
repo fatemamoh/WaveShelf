2366e6b0ed41ad07be11089b866fc49fac91a533
fatemamoh <fatimabuzuhaira@gmail.com>
2025-12-13 19:25:52 +0300

    add contoller and model to serve.js

---
+++ controllers/reviews.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const Book = require('../models/book');
+const Review = require('../models/review');
+
+
+module.exports = router;
